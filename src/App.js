2ee4d3830c5b4c1857647d351c437b276ebe1a0a
Vitalya-Str <114092416+Vitalya-Str@users.noreply.github.com>
2025-01-11 23:06:03 +0500

    #4 component Pizza

--- src/App.js
+++ src/App.js
@@ -3,23 +3,21 @@ import { Categories } from "./Components/Categories";
 import { Header } from "./Components/Header";
 import { PizzaBlock } from "./Components/PizzaBlock";
 import { Sort } from "./Components/Sort";
+import pizzes from '../src/assets/pizzes.json'
 
 function App() {
   return (
-    <div class="wrapper">
+    <div className="wrapper">
       <Header />
-      <div class="content">
-        <div class="container">
-          <div class="content__top">
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
             <Categories />
             <Sort />
           </div>
-          <h2 class="content__title">Все пиццы</h2>
-          <div class="content__items">
-            <PizzaBlock />
-            <PizzaBlock />
-            <PizzaBlock />
-            <PizzaBlock />
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            {pizzes.map((pizza, i) => <PizzaBlock key={i} {...pizza}  />)}
           </div>
         </div>
       </div>
--- src/Components/Categories.jsx
+++ src/Components/Categories.jsx
@@ -1,17 +1,23 @@
-
+import { useState } from "react";
 
 export const Categories = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const categories = ["Мясные", "Вегетарианская", "Гриль", "Острые", "Закрытые"];
+
+  const onCategories = (index) => {
+    setActiveIndex(index);
+  };
 
   return (
-    <div class="categories">
+    <div className="categories">
       <ul>
-        <li class="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categories.map((category, i) => (
+          <li key={i} onClick={() => onCategories(i)} className={activeIndex === i ? "active" : ""}>
+            {category}
+          </li>
+        ))}
       </ul>
     </div>
   );
-}
+};
